2e809a3b73ae18ad4978d55291dfee1b5d7f1528
Kornull <u.karnialiuk@gmail.com>
2023-10-09 14:45:38 +0300

    feat: add search components

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, SearchComponent, SearchResultsComponent, SearchItemComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
