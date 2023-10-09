b7e5ca72fcac3cdcf4a3913d292552bfb047f526
Kornull <u.karnialiuk@gmail.com>
2023-10-09 14:51:45 +0300

    feat: add models

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SearchComponent } from './search/search.component';
 import { SearchResultsComponent } from './search/search-results/search-results.component';
 import { SearchItemComponent } from './search/search-item/search-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchComponent, SearchResultsComponent, SearchItemComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ youtube-client-app/src/app/model/search-item.model.ts
@@ -0,0 +1,48 @@
+interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface ThumbnailsData {
+  default: Thumbnail;
+  medium: Thumbnail;
+  high: Thumbnail;
+  standard: Thumbnail;
+  maxres: Thumbnail;
+}
+
+interface Localized {
+  title: string;
+  description: string;
+}
+
+interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: ThumbnailsData;
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: Localized;
+  defaultAudioLanguage: string;
+}
+
+interface Statistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
+}
+
+export interface SearchItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+  statistics: Statistics;
+}
