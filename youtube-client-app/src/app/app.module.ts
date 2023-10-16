bc18bb3a3279f8a9f9d8981990b945c7616e3aad
Kornull <u.karnialiuk@gmail.com>
2023-10-16 19:43:02 +0300

    feat: add components folder

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SearchResultsComponent } from './search/search-results/search-results.component';
-import { SearchItemComponent } from './search/search-item/search-item.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchResultsComponent } from './components/search/search-results/search-results.component';
+import { SearchItemComponent } from './components/search/search-item/search-item.component';
 
 @NgModule({
   declarations: [
