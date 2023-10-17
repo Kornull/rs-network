401aa9a1615b659bcdae7f32358004c6175a3e4d
Kornull <u.karnialiuk@gmail.com>
2023-10-17 19:56:27 +0300

    feat: add header-search component

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchResultsComponent } from './components/search/search-results/search-results.component';
 import { SearchItemComponent } from './components/search/search-item/search-item.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderSearchComponent } from './components/header/header-search/header-search.component';
 
 @NgModule({
   declarations: [
@@ -13,8 +21,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HeaderComponent,
     SearchResultsComponent,
     SearchItemComponent,
+    HeaderSearchComponent,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ youtube-client-app/src/app/components/header/header-search/header-search.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-header-search',
+  templateUrl: './header-search.component.html',
+  styleUrls: ['./header-search.component.scss'],
+})
+export class HeaderSearchComponent {
+  inputValue: string = '';
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'logoIcon',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/logo/logo.svg')
+    );
+  }
+}
