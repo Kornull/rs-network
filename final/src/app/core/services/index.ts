edbbdb496325b48f97256c5cd6c3f65769e5f48e
Kornull <u.karnialiuk@gmail.com>
2023-12-15 20:36:56 +0300

    feat: add timer

--- final/src/app/core/services/index.ts
+++ final/src/app/core/services/index.ts
@@ -3,3 +3,4 @@ export * from './auth/auth.service';
 export * from './snack-bar/snack-bar.service';
 export * from './local-storage/local-storage.service';
 export * from './profile-data/profile-data.service';
+export * from './timer/timer.service';
--- final/src/app/pages/main/components/main-groups/main-groups.component.ts
+++ final/src/app/pages/main/components/main-groups/main-groups.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import { MatButtonModule } from '@angular/material/button';
+
+import { GROUPS } from './groups';
+import { GroupsData } from '../../../../core/store/models';
+import { TimerService } from '../../../../core/services';
 
 @Component({
   selector: 'app-main-groups',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, AsyncPipe],
   templateUrl: './main-groups.component.html',
   styleUrl: './main-groups.component.scss',
 })
-export class MainGroupsComponent {}
+export class MainGroupsComponent implements OnInit {
+  groups: GroupsData = GROUPS;
+
+  timeNow$!: Observable<number>;
+
+  disabledBtn$!: Observable<boolean>;
+
+  seconds = 60;
+
+  constructor(private timerService: TimerService) {}
+
+  ngOnInit(): void {
+    this.timeNow$ = this.timerService.getCountdown();
+    this.disabledBtn$ = this.timerService.getRunTimer();
+  }
+
+  updateList() {
+    this.timerService.startCountdown();
+  }
+}
